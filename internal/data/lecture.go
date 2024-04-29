a450506f617b1505504cac1446ce69f62d6725f3
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-04-29 09:55:52 +0500

    add validation lecture

--- internal/data/lecture.go
+++ internal/data/lecture.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"TalkRater_Bot/internal/validator"
+	"time"
+)
 
 type Lecture struct {
 	Id      int64
@@ -12,4 +15,13 @@ type Lecture struct {
 
 func (l *Lecture) Duration() time.Duration {
 	return l.End.Sub(l.Start)
+}
+
+func ValidateLecture(v *validator.Validator, lect *Lecture) {
+	v.Check(lect.Name != "", "name", "Name is required")
+	v.Check(lect.Speaker != "", "speaker", "Speaker is required")
+	v.Check(!lect.Start.IsZero(), "start", "Start time is required")
+	v.Check(!lect.End.IsZero(), "end", "End time is required")
+
+	v.Check(lect.Start.Before(lect.End), "start and end time", "Start time must be before End time")
 }
