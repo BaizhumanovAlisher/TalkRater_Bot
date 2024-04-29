aa1cab42e7719b96ec1ddd6bcd49d3c62a764b75
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-04-29 09:52:45 +0500

    add validation conference

--- internal/data/conference.go
+++ internal/data/conference.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"TalkRater_Bot/internal/validator"
+	"time"
+)
 
 type Conference struct {
 	Name              string    `json:"name"`
@@ -8,4 +11,16 @@ type Conference struct {
 	StartTime         time.Time `json:"start"`
 	EndTime           time.Time `json:"end"`
 	EndEvaluationTime time.Time `json:"end_evaluation"`
+}
+
+func ValidateConference(v *validator.Validator, conf *Conference) {
+	v.Check(conf.Name != "", "name", "Name is required")
+	v.Check(conf.URL != "", "url", "URL is required")
+
+	v.Check(!conf.StartTime.IsZero(), "start", "Start time is required")
+	v.Check(!conf.EndTime.IsZero(), "end", "End time is required")
+	v.Check(!conf.EndEvaluationTime.IsZero(), "end evaluation", "End evaluation time is required")
+
+	v.Check(conf.StartTime.Before(conf.EndTime), "start and end time", "Start time must be before End time")
+	v.Check(conf.EndTime.Before(conf.EndEvaluationTime), "end and end evaluation time", "End time must be before End evaluation time")
 }
