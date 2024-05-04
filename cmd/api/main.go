55e246ebd86ae33cdbd5bf5a235317630a5ce574
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-05-04 18:23:09 +0500

    improve logging in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,21 +13,26 @@ import (
 	"talk_rater_bot/internal/templates/user"
 )
 
+const op = "main.main"
+
 func main() {
 	cfg := helpers.MustLoadConfig()
 
 	logger := helpers.SetupLogger(cfg.Env, cfg.EnvVars.PathTmp)
 	slog.SetDefault(logger)
+	logger.Info(op, slog.String("info", "Start building application..."))
 
 	userBot := setupBot(cfg.TgBotSettings.TokenUser, cfg.TgBotSettings.Timeout)
 	adminBot := setupBot(cfg.TgBotSettings.TokenAdminPanel, cfg.TgBotSettings.Timeout)
 
 	adminTemplates, err := templates.NewTemplates(cfg.EnvVars.TemplatePath, admin.DirectoryName, admin.FilesName)
 	if err != nil {
+		logger.Warn(op, slog.String("error", err.Error()))
 		log.Fatal(err)
 	}
 	userTemplates, err := templates.NewTemplates(cfg.EnvVars.TemplatePath, user.DirectoryName, user.FilesName)
 	if err != nil {
+		logger.Warn(op, slog.String("error", err.Error()))
 		log.Fatal(err)
 	}
 
@@ -39,6 +44,7 @@ func main() {
 	dbHelper := databases.NewPrepareDBHelper(db, cfg.Conference, cfg.CleanupDBForNewConference, &cfg.DatabaseConfig, cfg.EnvVars.PathTmp)
 	err = dbHelper.PrepareDB()
 	if err != nil {
+		logger.Warn(op, slog.String("error", err.Error()))
 		log.Fatal(err)
 	}
 
@@ -54,8 +60,8 @@ func main() {
 	app.routes()
 	app.run()
 
-	logger.Info("Start application...")
-	logger.Info("conference",
+	logger.Info(op, slog.String("info", "Start application..."))
+	logger.Info(op, slog.String("info", "conference"),
 		slog.String("name", cfg.Conference.Name),
 		slog.String("url", cfg.Conference.URL),
 		slog.Time("start", cfg.Conference.StartTime),
@@ -65,9 +71,21 @@ func main() {
 
 	_ = helpers.WaitSignal()
 
-	logger.Info("Stop application...")
+	logger.Info(op, slog.String("info", "Stop application. Started to stop"))
+
 	app.userBot.Stop()
 	app.adminBot.Stop()
+	sqlDB, _ := db.DB()
+	if err != nil {
+		logger.Warn(op,
+			slog.String("info", "problem to close database connections"),
+			slog.String("error", err.Error()),
+		)
+	} else {
+		sqlDB.Close()
+	}
+
+	logger.Info(op, slog.String("info", "Stop application. Ended to stop"))
 }
 
 type application struct {
