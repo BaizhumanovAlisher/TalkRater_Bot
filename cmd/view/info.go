6d08ec29c0088be2709fe94e27c5c7f25a309409
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-05-07 21:22:43 +0500

    add conference

--- cmd/view/info.go
+++ cmd/view/info.go
@@ -1,10 +1,27 @@
 package view
 
-import tele "gopkg.in/telebot.v3"
+import (
+	tele "gopkg.in/telebot.v3"
+	"log/slog"
+	"talk_rater_bot/internal/data"
+	"talk_rater_bot/internal/helpers"
+	"talk_rater_bot/internal/templates"
+)
 
 const opViewConference = "info.viewConference"
 
-func (app *Application) viewConference(context tele.Context) error {
-	//todo:
-	panic("implement me")
+func (app *Application) viewConference(c tele.Context) error {
+	app.Logger.Info(opViewConference, slog.String("username", c.Sender().Username))
+
+	return c.Send(app.Templates.Render(templates.ConferenceTmpl,
+		&templates.TemplateData{Conference: convertConf(app.Conference, app.TimeParser)}))
+}
+
+func convertConf(conf *data.Conference, parser *helpers.TimeParser) *templates.Conference {
+	return &templates.Conference{
+		Name:      conf.Name,
+		URL:       conf.URL,
+		StartTime: parser.ConvertTime(conf.StartTime),
+		EndTime:   parser.ConvertTime(conf.EndTime),
+	}
 }
--- cmd/view/routes.go
+++ cmd/view/routes.go
@@ -5,19 +5,15 @@ import (
 )
 
 func (app *Application) Routes() {
-	app.adminBotRoutes()
-	app.userBotRoutes()
-}
-
-func (app *Application) userBotRoutes() {
 	app.UserBot.Use(app.recoverPanic, app.measureTime)
-}
 
-func (app *Application) adminBotRoutes() {
+	app.UserBot.Handle("/conference", app.viewConference)
+
 	app.AdminBot.Use(app.recoverPanic, app.measureTime, app.checkAdmin)
 
 	app.AdminBot.Handle("/start", app.startAndHelpAdmin)
 	app.AdminBot.Handle("/help", app.startAndHelpAdmin)
 	app.AdminBot.Handle(tele.OnDocument, app.submitSchedule)
 	app.AdminBot.Handle("/export", app.exportEvaluations)
+	app.AdminBot.Handle("/conference", app.viewConference)
 }
